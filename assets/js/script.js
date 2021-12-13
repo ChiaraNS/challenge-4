9df70743e397f1125dc4c74f66be1bf82906bc75
Key <cns3.22202@gmail.com>
2021-12-13 01:28:09 -0600

    questions and answer choices in JS

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+var questions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        a: "1. Strings",
+        b: "2. Booleans",
+        c: "3. Alerts",
+        d: "4. Numbers",
+        ans: "3. Alerts",
+    },
+    {
+        question: "The condition in an if/an statement is enclosed with ____.",
+        a: "1. Quotes",
+        b: "2. Curly Brackets",
+        c: "3. Parenthesis",
+        d: "4. Square Brackets",
+        ans: "3. Parenthesis",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ___.",
+        a: "1. Numbers and strings ",
+        b: "2. Other arrays",
+        c: "3. Booleans",
+        d: "4. All of the above",
+        ans: "4. All of the above",
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        a: "1. Commas",
+        b: "2. Curly brackets",
+        c: "3. Quotes",
+        d: "4. Parenthesis",
+        ans: "3. Quotes",
+    },
+    {
+        question: "A very useful tool during development and debugging for printing content to the debugger is:",
+        a: "1. JavaScript",
+        b: "2. Terminal/bash",
+        c: "3. For loops",
+        d: "4. Console.log",
+        ans: "4. Console.log",
+    },
+
+]
